f1e7a5d78db1013215c779b9bc14296da7445f78
Lugas90 <Lugas90@mail.ru>
2022-10-27 17:18:19 +0300

    комит презы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int x = 0;
+        int y = 30;
+
+        try {
+            int num = y/x;
+        } catch (ArithmeticException | IndexOutOfBoundsException | IllegalArgumentException e){
+            System.out.println(e);
+        }
     }
 }
